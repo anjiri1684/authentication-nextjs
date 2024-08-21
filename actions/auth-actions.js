bd26355c17ee8b3789a5bca0055251f344f499dc
vincent anjiri <vincentanjiri12@gmail.com>
2024-08-21 23:33:18 +0300

    "Added logout functionality, removed TrainingPage, and updated auth.js to include destroySession function"

--- actions/auth-actions.js
+++ actions/auth-actions.js
@@ -1,6 +1,6 @@
 "use server";
 
-import { createAuthSession } from "@/lib/auth";
+import { createAuthSession, destroySession } from "@/lib/auth";
 import { hashUserPassword, verifyPassword } from "@/lib/hash";
 import { createUser, getUserByEmail } from "@/lib/user";
 import { redirect } from "next/navigation";
@@ -75,4 +75,10 @@ export async function auth(mode, prevState, formData) {
     return login(prevState, formData);
   }
   return signup(prevState, formData);
+}
+
+export async function logout() {
+  // Clear the auth session
+  await destroySession();
+  redirect("/");
 }
